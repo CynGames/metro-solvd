ada0b3de05be9126397107febaffedc51ec8989c
CynGames <tomasm.leguizamon@gmail.com>
2023-05-13 14:19:56 -0300

    Fix for wrong runner in github actions

--- config/config.js
+++ config/config.js
@@ -1,10 +1,30 @@
 require('dotenv').config();
+const url = require('url');
 
-module.exports = {
-    secret: process.env.SECRET,
-    port: +process.env.PORT,
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-};
+let config;
+if (process.env.DATABASE_URL) {
+    const params = url.parse(process.env.DATABASE_URL);
+    const auth = params.auth.split(':');
+
+    config = {
+        user: auth[0],
+        password: auth[1],
+        host: params.hostname,
+        port: params.port,
+        database: params.pathname.split('/')[1],
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    };
+} else {
+    config = {
+        secret: process.env.SECRET,
+        port: +process.env.PORT,
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE,
+    };
+}
+
+module.exports = config;
